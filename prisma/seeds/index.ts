2f6e606fd0399327547a6b175d6253bfb7036845
Eclipsian <erica2007210@gmail.com>
2022-09-04 18:21:02 -0400

    cw5

--- prisma/seeds/index.ts
+++ prisma/seeds/index.ts
@@ -1,13 +1,45 @@
-import {
-  Gender,
-  ListingCategory,
-  ListingSubCategory,
-  NewsCategory,
-  PrismaClient,
-} from '@prisma/client';
+import { PrismaClient, } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 
-export const seedListings = async (prisma: PrismaClient) => {
+export const seeding = async (prisma: PrismaClient) => {
   console.log('Seeding start...');
   const now = new Date();
+
+  const exampleStudent = await prisma.student.create({
+    data: {
+      firstName: 'Kevin',
+      lastName: 'Zhang',
+      age: 10,
+      grade: 'A',
+    },
+  });
+
+  const exampleProduct = await prisma.product.create({
+    data: {
+      title: 'iPhone 14',
+      price: 6572,
+      quantity: 48973,
+    }
+  })
+
+  const order = await prisma.order.create({
+    data: {
+      student: {
+        connect: {
+          id: exampleStudent.id,
+        },
+      },
+      products: {
+        connect: [
+          {
+            id: exampleProduct.id,
+          },
+          {
+            //can have another product
+          }
+        ],
+      },
+    },
+  });
+
 };
