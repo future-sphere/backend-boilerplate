095be3c4d56eeec878f56e2f3c7eac216819abb3
mxdi9i7 <peter.zheng88228@gmail.com>
2023-04-16 22:14:00 -0400

    updates

--- src/controllers/cart/index.ts
+++ src/controllers/cart/index.ts
@@ -10,6 +10,23 @@ import { prisma } from '../..';
 
 export const addItemToCart = async (req: Request, res: Response) => {
   const { productId, quantity, userId } = req.body;
+
+  const studentExists = await prisma.student.findUnique({
+    where: {
+      id: +userId,
+    },
+  });
+
+  if (!studentExists) {
+    return res.status(404).json({ message: 'Student not found' });
+  }
+
+  // Create a check to check if the quantity is at least 1, if not, return an error
+  // message saying that the quantity must be at least 1
+  if (quantity < 1) {
+    return res.status(400).json({ message: 'Quantity must be at least 1' });
+  }
+
   const product = await prisma.product.findUnique({
     where: {
       id: +productId,
@@ -104,6 +121,74 @@ export const addItemToCart = async (req: Request, res: Response) => {
         },
       });
     }
+  }
+
+  return res.json(cart);
+};
+
+export const updateCartItemQuantity = async (req: Request, res: Response) => {
+  const { cartItemId, quantity } = req.body;
+
+  if (quantity < 0) {
+    return res.status(400).json({ message: 'Quantity must be at least 1' });
+  }
+
+  const cartItem = await prisma.cartItem.findUnique({
+    where: {
+      id: +cartItemId,
+    },
+  });
+
+  if (!cartItem) {
+    return res.status(404).json({ message: 'Cart item not found' });
+  }
+
+  const cart = await prisma.cart.update({
+    where: {
+      id: cartItem.cartId,
+    },
+    data: {
+      cartItems:
+        quantity === 0
+          ? {
+              delete: {
+                id: +cartItemId,
+              },
+            }
+          : {
+              update: {
+                where: {
+                  id: +cartItemId,
+                },
+                data: {
+                  quantity: +quantity,
+                },
+              },
+            },
+    },
+    include: {
+      cartItems: true,
+    },
+  });
+
+  return res.json(cart);
+};
+
+// Write a controller that takes in a cartId and returns the cart with all the cart items
+export const getCartByStudent = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const cart = await prisma.cart.findFirst({
+    where: {
+      studentId: +id,
+    },
+    include: {
+      cartItems: true,
+    },
+  });
+
+  if (!cart) {
+    return res.status(404).json({ message: 'Cart not found' });
   }
 
   return res.json(cart);
--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
-import { addItemToCart } from '../controllers/cart';
+import {
+  addItemToCart,
+  getCartByStudent,
+  updateCartItemQuantity,
+} from '../controllers/cart';
 
 const cartRouter = Router();
 
 cartRouter.post('/', addItemToCart);
+cartRouter.put('/quantity', updateCartItemQuantity);
+cartRouter.get('/:id/student', getCartByStudent);
 
 export default cartRouter;
